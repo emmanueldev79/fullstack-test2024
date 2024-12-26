4206363b113b527c78ebfef33797b51122ed4b5d
Emmanuel Montoya Lopez <emmanuelprogramador24@gmail.com>
2024-12-26 16:14:20 -0500

    Subiendo test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5174', // Cambia esto al puerto o dominio de tu frontend
+    origin: 'https://reliable-pudding-fd454a.netlify.app/', // Cambia esto al puerto o dominio de tu frontend
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, // Si necesitas enviar cookies o encabezados de autenticación
   });
